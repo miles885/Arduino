2cc0e0511b748cfb33d470262249e74c2ba67007
Miles Gapcynski <miles885@gmail.com>
2019-09-07 13:52:03 -0400

    Cleaned up code and getting ready to output morse code.

--- Module1/Module1.ino
+++ Module1/Module1.ino
@@ -9,7 +9,7 @@
 */
 
 /* Constants */
-const int LED_PIN = 13;
+const int OUTPUT_PIN = 13;
 
 const int ETX = 3;  // CTRL + C
 const int LF = 10;
@@ -21,6 +21,46 @@ int SENTINELS[] = {ETX, SUB};
 const int SERIAL_BUFFER_SIZE = 64;
 const int INPUT_BUFFER_SIZE = SERIAL_BUFFER_SIZE * 10;
 
+const int MORSE_CODE_TIME_UNIT = 1000;  // Milliseconds
+
+char * MORSE_CODE_CHARS[] = {".-",     // A 
+                             "-...",   // B
+                             "-.-.",   // C
+                             "-..",    // D
+                             ".",      // E
+                             "..-.",   // F
+                             "--.",    // G
+                             "....",   // H
+                             "..",     // I
+                             ".---",   // J
+                             "-.-",    // K
+                             ".-..",   // L
+                             "--",     // M
+                             "-.",     // N
+                             "---",    // O
+                             ".--.",   // P
+                             "--.-",   // Q
+                             ".-.",    // R
+                             "...",    // S
+                             "-",      // T
+                             "..-",    // U
+                             "...-",   // V
+                             ".--",    // W
+                             "-..-",   // X
+                             "-.--",   // Y
+                             "--.."};  // Z
+
+char * MORSE_CODE_NUMS[] = {"-----",   // 0
+                            ".----",   // 1
+                            "..---",   // 2
+                            "...--",   // 3
+                            "....-",   // 4
+                            ".....",   // 5
+                            "-....",   // 6
+                            "--...",   // 7
+                            "---..",   // 8
+                            "----."};  // 9
+
 /**
  * Initialization that gets run when you press reset or power the board
  * 
@@ -30,11 +70,11 @@ const int INPUT_BUFFER_SIZE = SERIAL_BUFFER_SIZE * 10;
  */
 void setup()
 {
-  // Initialize pins
-  pinMode(LED_PIN, OUTPUT);
+    // Initialize pins
+    pinMode(LED_PIN, OUTPUT);
 
-  // Setup up the serial connection at 9600 bits per second
-  Serial.begin(9600);
+    // Setup up the serial connection at 9600 bits per second
+    Serial.begin(9600);
 }
 
 /**
@@ -46,41 +86,92 @@ void setup()
  */
 bool getUserInput(char * userInput)
 {
-  static int charIndex = 0;
+    static int charIndex = 0;
   
-  while(Serial.available())
-  {
-    // Read a character from the serial buffer
-    char serialInput = Serial.read();
-
-    //TODO: Prevent empty strings (hitting enter with nothing else)
-    // Check to see if all user input has been retrieved or input buffer is full
-    if(serialInput == LF || serialInput == CR || charIndex == INPUT_BUFFER_SIZE)
+    while(Serial.available())
     {
-      charIndex = 0;
+        // Read a character from the serial buffer
+        char serialInput = Serial.read();
+
+        // Check to see if all user input has been retrieved or input buffer is full
+        if(serialInput == LF || serialInput == CR || charIndex == INPUT_BUFFER_SIZE)
+        {
+            charIndex = 0;
       
-      return true;
+            return true;
+        }
+
+        // Check to see if a sentinel was passed in
+        int numSentinels = sizeof(SENTINELS) / sizeof(int);
+
+        for(int i = 0; i < numSentinels; i++)
+        {
+            if(serialInput == SENTINELS[i])
+            {
+                Serial.println("Received sentinel - ending program!");
+
+                //TODO: Need a system.exit(0) or something
+                break;
+            }
+        }
+
+        userInput[charIndex] = serialInput;
+        charIndex++;
     }
 
-    // Check to see if a sentinel was passed in
-    int numSentinels = sizeof(SENTINELS) / sizeof(int);
+    return false;
+}
 
-    for(int i = 0; i < numSentinels; i++)
+/**
+ * Outputs the user input as morse code using the output pin
+ * 
+ * @param userInput Character buffer storing the user input data
+ * 
+ * @return None
+ */
+void outputMorseCode(char * userInput)
+{
+    for(int i = 0; i < INPUT_BUFFER_SIZE; i++)
     {
-      if(serialInput == SENTINELS[i])
-      {
-        Serial.println("Received sentinel - ending program!");
+        // Check to see if end of string has been reached
+        if(userInput[i] == NULL)
+        {
+            return;
+        }
 
-        //TODO: Need a system.exit(0) or something
-        break;
-      }
+        // Check to see if the character is upper case
+        if(userInput[i] >= 'A' && userInput[i] <= 'Z')
+        {
+            
+        }
+        // Check to see if the character is lower case
+        else if(userInput[i] >= 'A' && userInput[i] <= 'Z')
+        {
+            
+        }
+        // Check to see if the character is a number
+        else if(userInput[i] >= '0' && userInput[i] <= '9')
+        {
+            
+        }
+        // Check to see if the character is a space
+        else if(userInput[i] == ' ')
+        {
+            
+        }
     }
+    //digitalWrite(OUTPUT_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    //delay(1000);                       // wait for a second
+    //digitalWrite(OUTPUT_PIN, LOW);    // turn the LED off by making the voltage LOW
+    //delay(1000);                       // wait for a second
+}
 
-    userInput[charIndex] = serialInput;
-    charIndex++;
-  }
-
-  return false;
+/**
+ * 
+ */
+void outputSequence(char * codeSequence)
+{
+    
 }
 
 /**
@@ -92,10 +183,10 @@ bool getUserInput(char * userInput)
  */
 void flushSerialBuffer()
 {
-  while(Serial.available())
-  {
-    Serial.read();
-  }
+    while(Serial.available())
+    {
+        Serial.read();
+    }
 }
 
 /**
@@ -107,40 +198,29 @@ void flushSerialBuffer()
  */
 void loop()
 {
-  static char userInput[INPUT_BUFFER_SIZE + 1];
-  static bool needNewString = true;
+    static char userInput[INPUT_BUFFER_SIZE + 1];
+    static bool needNewString = true;
   
-  // Prompt the user to enter a string
-  if(needNewString == true)
-  {
-    Serial.println("Please type something to output as morse code (max " + String(INPUT_BUFFER_SIZE) + " characters).");
-
-    memset(userInput, 0, sizeof(userInput));
-    needNewString = false;
-  }
-
-  // Retrieve the user input
-  if(Serial.available())
-  {
-    bool hasAllUserInput = getUserInput(userInput);
-
-    if(hasAllUserInput)
+    // Prompt the user to enter a string
+    if(needNewString == true)
     {
-      Serial.println("Your user input:");
-      Serial.println(userInput);
-      Serial.println("Now has all user input data!");
+        Serial.println("Please type something to output as morse code (max " + String(INPUT_BUFFER_SIZE) + " characters).");
+
+        memset(userInput, 0, sizeof(userInput));
+        needNewString = false;
     }
 
-    //digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    //delay(1000);                       // wait for a second
-    //digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
-    //delay(1000);                       // wait for a second
-  
-    if(hasAllUserInput)
+    // Retrieve the user input
+    if(Serial.available())
     {
-      needNewString = true;
+        bool hasAllUserInput = getUserInput(userInput);
 
-      flushSerialBuffer();
+        if(hasAllUserInput)
+        {
+            needNewString = true;
+            
+            outputMorseCode();
+            flushSerialBuffer();
+        }
     }
-  }
 }
