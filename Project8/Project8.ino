6d482e4a20979bf908065f43221b7d04e5d93759
Miles Gapcynski <miles885@gmail.com>
2019-11-10 15:44:11 -0500

    Now using FreeRTOS to read and transmit BNO055 data.

--- Project8/Project8.ino
+++ Project8/Project8.ino
@@ -7,15 +7,15 @@
   pitch, and yaw from the Quaternion data, and transmits the data over serial
 */
 
-#include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
+#include <Arduino_FreeRTOS.h>
 #include <utility/imumaths.h>
+#include <Wire.h>
 
 /* Constants */
 const bool CALIBRATING_BNO = false;
 
-const float RAD_TO_DEG = 57.2958;  // 57.2958 = 180 / pi
 const int BNO_SAMPLE_RATE = 100;  // ms
 
 const adafruit_bno055_offsets_t SENSOR_OFFSETS = {-19, -32, -35,  // Acceleration
@@ -25,8 +25,6 @@ const adafruit_bno055_offsets_t SENSOR_OFFSETS = {-19, -32, -35,  // Acceleratio
                                                   805};           // Magnetometer radius
 
 /* Global variables */
-//TODO: BNO055_ID, BNO055_ADDRESS_A
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 /**
  * Initialization that gets run when you press reset or power the board
@@ -41,6 +39,78 @@ void setup()
     Serial.begin(115200);
     delay(1000);
 
+    // Create tasks
+    xTaskCreate(TaskReadIMU, "ReadIMU", 256, NULL, 2, NULL);
+}
+
+/**
+ * Runs indefinitely (not used by FreeRTOS)
+ * 
+ * @param None
+ * 
+ * @return None
+ */
+void loop()
+{
+    
+}
+
+/**
+ * Task entry point for configuring the BNO055 sensor, reading
+ * data off the sensor, and transmitting roll, pitch, yaw over serial
+ * 
+ * @param pvParameters Task parameters
+ * 
+ * @return None
+ */
+void TaskReadIMU(void * pvParameters)
+{
+    //TODO: BNO055_ID, BNO055_ADDRESS_A
+    Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+    
+    initBNOSensor(bno);
+
+    while(true)
+    {
+        // Retrieve quaternion
+        imu::Quaternion quat = bno.getQuat();
+    
+        float w = quat.w();
+        float x = quat.x();
+        float y = quat.y();
+        float z = quat.z();
+    
+        // Calculate RPY
+        float roll = atan2(2.0 * (w * x + y * z), 1.0 - 2.0 * (x * x + y * y));
+        float pitch = asin(2.0 * w * y - x * z);
+        float yaw = atan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z));
+    
+        float rollDeg = roll * RAD_TO_DEG;
+        float pitchDeg = pitch * RAD_TO_DEG;
+        float yawDeg = yaw * RAD_TO_DEG;
+    
+        // Transmit RPY over serial (USB)
+        //TODO: Replace Serial with Wire library when using I2C (probably need separate task to handle I2C requests too)
+        Serial.print(rollDeg);
+        Serial.print(",");
+        Serial.print(pitchDeg);
+        Serial.print(",");
+        Serial.print(yawDeg);
+        Serial.println("");
+        
+        delay(BNO_SAMPLE_RATE);
+    }
+}
+
+/**
+ * Initialize the BNO055 sensor
+ * 
+ * @param bno The BNO055 sensor object
+ * 
+ * @return None
+ */
+void initBNOSensor(Adafruit_BNO055 & bno)
+{
     // Initialize the BNO055 sensor
     if(!bno.begin(Adafruit_BNO055::OPERATION_MODE_NDOF))
     {
@@ -83,7 +153,7 @@ void setup()
         {
             bno.getEvent(&event);
             
-            getCalData();
+            getCalData(bno);
 
             delay(BNO_SAMPLE_RATE);
         }
@@ -94,11 +164,11 @@ void setup()
  * Prints calibration status and sensor offsets
  * when sensor is fully calibrated
  * 
- * @param None
+ * @param bno The BNO055 sensor object
  * 
  * @return None
  */
-void getCalData()
+void getCalData(Adafruit_BNO055 & bno)
 {
     uint8_t system, gyro, accel, mag;
     bno.getCalibration(&system, &gyro, &accel, &mag);
@@ -150,41 +220,4 @@ void getCalData()
         Serial.print(sensorOffsets.mag_radius);
         Serial.println("");
     }
-}
-
-/**
- * Runs indefinitely
- * 
- * @param None
- * 
- * @return None
- */
-void loop()
-{
-    // Retrieve quaternion
-    imu::Quaternion quat = bno.getQuat();
-
-    float w = quat.w();
-    float x = quat.x();
-    float y = quat.y();
-    float z = quat.z();
-
-    // Calculate RPY
-    float roll = atan2(2.0 * (w * x + y * z), 1.0 - 2.0 * (x * x + y * y));
-    float pitch = asin(2.0 * w * y - x * z);
-    float yaw = atan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z));
-
-    float rollDeg = roll * RAD_TO_DEG;
-    float pitchDeg = pitch * RAD_TO_DEG;
-    float yawDeg = yaw * RAD_TO_DEG;
-
-    // Transmit RPY over serial (USB)
-    Serial.print(rollDeg);
-    Serial.print(",");
-    Serial.print(pitchDeg);
-    Serial.print(",");
-    Serial.print(yawDeg);
-    Serial.println("");
-    
-    delay(BNO_SAMPLE_RATE);
 }
